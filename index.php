41a393eb6be5bcf49e26d1a8ecd29621346a6527
Prathyoush M Tom <51507732+prathyoush@users.noreply.github.com>
2021-03-12 08:55:26 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  
+    // By Qassim Hassan | wp-time.com
+  
+    session_start();
+ 
+    $_SESSION['prevent_repeat'] = rand(); // Prevent a repeat of the process
+ 
+?>
+ 
+<form method="post" action="shortener.php">
+    <p>Enter a long link: <input type="text" name="my-link" value=""></p>
+    <p><input type="submit" value="Shorten!"></p>
+</form>
